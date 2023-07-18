47c7b8feeed679c7d3ca277110e1bcbadc775c9b
Ingiboy <fatkulin.igor@gmail.com>
2023-07-19 00:14:22 +0600

    svn & git creds

---
+++ svn-git-sync.py
@@ -0,0 +1,20 @@
+### SVN
+# svn://192.168.0.98/repo/factor-test
+# user: user
+# pass: pass
+
+### Создать новый репозиторий из командной строки
+# user: giteuser
+# pass: passpass
+# touch README.md
+# git init
+# git checkout -b main
+# git add README.md
+# git commit -m "first commit"
+# git remote add origin http://192.168.0.98:3000/factor/test.git
+# git push -u origin main
+#
+### Отправка существующего репозитория из командной строки
+# git remote add origin http://192.168.0.98:3000/factor/test.git
+# git push -u origin main
+
